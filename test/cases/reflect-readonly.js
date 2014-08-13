faf37b693b9833a827c3408c1702486d87c777fa
Domenic Denicola <domenic@domenicdenicola.com>
2014-08-12 20:20:14 -0400

    Automatically lower-case the attribute name for [Reflect]
    That way you don't need to do [Reflect=noShade] and such. This matches WebKit/Blink, see e.g. https://code.google.com/p/chromium/codesearch#chromium/src/third_party/WebKit/Source/core/html/HTMLHRElement.idl

--- test/cases/reflect-readonly.js
+++ test/cases/reflect-readonly.js
@@ -1,6 +1,6 @@
 import reflector from "webidl-reflector";
 export default class ReflectReadonly {
   get reflectMe() {
-    return reflector["unsigned long"](this, "reflectMe");
+    return reflector["unsigned long"](this, "reflectme");
   }
 }
