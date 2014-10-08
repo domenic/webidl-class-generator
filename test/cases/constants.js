3e9757ba3cd955316bce6c1e9cfefcb8b4ecba8f
Domenic Denicola <domenic@domenicdenicola.com>
2014-10-08 15:55:28 -0400

    Add support for constants
    Closes #14

---
+++ test/cases/constants.js
@@ -0,0 +1,18 @@
+var defineProperty = Object.defineProperty;
+export default class Constants {}
+defineProperty(Constants, "ZERO", {
+  value: 0,
+  enumerable: true
+});
+defineProperty(Constants.prototype, "ZERO", {
+  value: 0,
+  enumerable: true
+});
+defineProperty(Constants, "ONE", {
+  value: 1,
+  enumerable: true
+});
+defineProperty(Constants.prototype, "ONE", {
+  value: 1,
+  enumerable: true
+});
